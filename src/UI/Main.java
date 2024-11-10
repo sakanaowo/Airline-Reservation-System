93943d239427b4278c0013e3b38ead25cb719eab
sakanaowo <anhlandibo88@gmail.com>
2024-11-10 22:24:50 +0700

    rework UI

--- src/UI/Main.java
+++
@@ -1,9 +0,0 @@
-package UI;
-
-import javax.swing.SwingUtilities;
-//test
-public class Main {
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(LoginWindow::new);
-    }
-}
