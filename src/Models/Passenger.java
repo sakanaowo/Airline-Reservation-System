8e79f95eb83620d4e14e8cdef3fddb64c4a5f306
sakanaowo <anhlandibo88@gmail.com>
2024-11-10 23:38:09 +0700

    chỉnh passenger theo DB

--- src/Models/Passenger.java
+++ src/Models/Passenger.java
@@ -1,44 +1,52 @@
 package Models;
 
 public class Passenger {
-    private String name, birthDate, ethnicity, ID;
+    private int PassengerID;
+    private String LastName, FirstName;
+    int UserID;
+    private String PhoneNumber, CitizenIdentificationNumber;
 
-    public Passenger(String name, String birthDate, String ethnicity, String ID) {
-        this.name = name;
-        this.birthDate = birthDate;
-        this.ethnicity = ethnicity;
-        this.ID = ID;
+    public Passenger(String FirstName, String LastName, int UserID, String PhoneNumber, String CitizenIdentificationNumber) {
+        this.FirstName = FirstName;
+        this.LastName = LastName;
+        this.UserID = UserID;
+        this.PhoneNumber = PhoneNumber;
+        this.CitizenIdentificationNumber = CitizenIdentificationNumber;
+    }
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String FirstName) {
+        this.FirstName = FirstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String LastName) {
+        this.LastName = LastName;
+    }
+
+    public String getCitizenID() {
+        return CitizenIdentificationNumber;
+    }
+
+    public void setCitizenID(String PassengerID) {
+        this.CitizenIdentificationNumber = PassengerID;
+    }
+
+    public String getPhoneNumber() {
+        return PhoneNumber;
+    }
+
+    public void setPhoneNumber(String PhoneNumber) {
+        this.PhoneNumber = PhoneNumber;
     }
 
     public String getName() {
-        return name;
-    }
-
-    public String getID() {
-        return ID;
-    }
-
-    public String getBirthDate() {
-        return birthDate;
-    }
-
-    public String getEthnicity() {
-        return ethnicity;
-    }
-    //Setter dùng để sửa thông tin(Nếu có nhu cầu)
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setBirthDate(String birthDate) {
-        this.birthDate = birthDate;
-    }
-
-    public void setEthnicity(String ethnicity) {
-        this.ethnicity = ethnicity;
-    }
-
-    public void setID(String ID) {
-        this.ID = ID;
+        return FirstName + " " + LastName;
     }
 }
