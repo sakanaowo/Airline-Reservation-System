75a7ca4267ef841c659ca20e7732f6c8b7cc13de
sakanaowo <anhlandibo88@gmail.com>
2024-11-11 21:51:01 +0700

    add Admin

---
+++ src/Models/Admin.java
@@ -0,0 +1,40 @@
+package Models;
+
+public class Admin {
+    private int admin_id;
+    private String admin_name;
+    private String admin_password;
+    private String admin_email;
+
+    public int getAdmin_id() {
+        return admin_id;
+    }
+
+    public void setAdmin_id(int admin_id) {
+        this.admin_id = admin_id;
+    }
+
+    public String getAdmin_name() {
+        return admin_name;
+    }
+
+    public void setAdmin_name(String admin_name) {
+        this.admin_name = admin_name;
+    }
+
+    public String getAdmin_password() {
+        return admin_password;
+    }
+
+    public void setAdmin_password(String admin_password) {
+        this.admin_password = admin_password;
+    }
+
+    public String getAdmin_email() {
+        return admin_email;
+    }
+
+    public void setAdmin_email(String admin_email) {
+        this.admin_email = admin_email;
+    }
+}
