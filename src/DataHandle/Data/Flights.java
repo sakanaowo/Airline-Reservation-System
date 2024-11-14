46b9554800e483f49f3f8476d4eeca4dca9196f0
sakanaowo <anhlandibo88@gmail.com>
2024-11-14 15:34:12 +0700

    update

--- src/DataHandle/Data/Flights.java
+++ src/DataHandle/Data/Flights.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Data;
+package DataHandle.Data;
 import constants.CommonConstants;
 import java.sql.*;
 import java.util.*;
--- src/DataHandle/Data/Seats.java
+++ src/DataHandle/Data/Seats.java
@@ -2,8 +2,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Data;
-import constants.CommonConstants;
+package DataHandle.Data;
+
+import DataHandle.constants.CommonConstants;
+
 import java.sql.*;
 import java.util.*;
 import java.time.*;
