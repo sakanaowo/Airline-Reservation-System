cd06b09e49cb3d2c02f7f9a61213fa1760e613d7
tan <tramita2712004@gmail.com>
2024-11-14 14:51:44 +0700

    rm

--- src/UI/main/Main.java
+++ src/UI/main/Main.java
@@ -2,9 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package UI.main;
+package main;
 
-/// @author ADMIN
+/**
+ *
+ * @author ADMIN
+ */
 public class Main {
 
     /**
