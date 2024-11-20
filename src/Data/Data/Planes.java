ce20c8a18173e2192ec84e3ad5234e4f0d4c6c35
thinhnguyenpy <thinhthiennguyen2004@gmail.com>
2024-11-20 17:47:11 +0700

    Sua kha nhieu do nhung khi lam den thi noi sau nhe

--- src/Data/Data/Planes.java
+++ src/Data/Data/Planes.java
@@ -12,35 +12,47 @@ import java.time.*;
  * @author DELL
  */
 public class Planes {
-        public static boolean insertPlane(String model, int seats, String updatedBy) {
+    public static boolean insertPlane(String model, int seats, String locationAirport, String updatedBy) {
         String insertPlaneSQL = "INSERT INTO " + CommonConstants.DB_PLANES_TABLE +
-        " (Model, Seats, UpdatedBy, UpdatedDate) VALUES (?, ?, ?, ?)";
+                " (Model, Seats, LocationID, UpdatedBy, UpdatedDate) VALUES (?, ?, ?, ?, ?)";
 
         try (Connection connection = DriverManager.getConnection(
-            CommonConstants.DB_URL, CommonConstants.DB_USERNAME, CommonConstants.DB_PASSWORD);
-            PreparedStatement adminIDStmt = connection.prepareStatement(
-             "SELECT AdminID FROM " + CommonConstants.DB_ADMIN_TABLE + " WHERE AdminName = ?");
-            PreparedStatement insertPlaneStmt = connection.prepareStatement(insertPlaneSQL)) {
+                CommonConstants.DB_URL, CommonConstants.DB_USERNAME, CommonConstants.DB_PASSWORD);
+             PreparedStatement adminIDStmt = connection.prepareStatement(
+                     "SELECT AdminID FROM " + CommonConstants.DB_ADMIN_TABLE + " WHERE AdminName = ?");
+             PreparedStatement locationIDStmt = connection.prepareStatement(
+                     "SELECT AirportID FROM " + CommonConstants.DB_AIRPORTS_TABLE + " WHERE AirportName = ?");
+             PreparedStatement insertPlaneStmt = connection.prepareStatement(insertPlaneSQL)) {
 
             adminIDStmt.setString(1, updatedBy);
-            ResultSet resultSet = adminIDStmt.executeQuery();
-            Integer adminID = null; 
-
-            if (resultSet.next()) {
-                adminID = resultSet.getInt("AdminID");
+            ResultSet adminResultSet = adminIDStmt.executeQuery();
+            Integer adminID = null;
+            if (adminResultSet.next()) {
+                adminID = adminResultSet.getInt("AdminID");
             } else {
-                System.err.println("Admin not found");
+                System.err.println("Admin not found: " + updatedBy);
+                return false;
+            }
+
+            locationIDStmt.setString(1, locationAirport);
+            ResultSet locationResultSet = locationIDStmt.executeQuery();
+            Integer locationID = null;
+            if (locationResultSet.next()) {
+                locationID = locationResultSet.getInt("AirportID");
+            } else {
+                System.err.println("Airport not found: " + locationAirport);
                 return false;
             }
 
             insertPlaneStmt.setString(1, model);
             insertPlaneStmt.setInt(2, seats);
+            insertPlaneStmt.setInt(3, locationID);
             if (adminID != null) {
-                insertPlaneStmt.setInt(3, adminID);
+                insertPlaneStmt.setInt(4, adminID);
             } else {
-                insertPlaneStmt.setNull(3, java.sql.Types.INTEGER);
+                insertPlaneStmt.setNull(4, java.sql.Types.INTEGER);
             }
-            insertPlaneStmt.setDate(4, new java.sql.Date(System.currentTimeMillis()));
+            insertPlaneStmt.setDate(5, new java.sql.Date(System.currentTimeMillis()));
 
             insertPlaneStmt.executeUpdate();
             return true;
@@ -51,36 +63,47 @@ public class Planes {
         }
     }
 
-    public static boolean modifyPlane(int planeID, String model, int seats, String updatedBy) {
+
+    public static boolean modifyPlane(int planeID, String model, int seats, String locationAirport, String updatedBy) {
         String modifyPlaneSQL = "UPDATE " + CommonConstants.DB_PLANES_TABLE +
-        " SET Model = ?, Seats = ?, UpdatedBy = ?, UpdatedDate = ? WHERE PlaneID = ?";
+                " SET Model = ?, Seats = ?, LocationID = ?, UpdatedBy = ?, UpdatedDate = ? WHERE PlaneID = ?";
 
         try (Connection connection = DriverManager.getConnection(
-            CommonConstants.DB_URL, CommonConstants.DB_USERNAME, CommonConstants.DB_PASSWORD);
-            PreparedStatement modifyPlaneStmt = connection.prepareStatement(modifyPlaneSQL);
-            PreparedStatement adminIDStmt = connection.prepareStatement(
-             "SELECT AdminID FROM " + CommonConstants.DB_ADMIN_TABLE + " WHERE AdminName = ?")) {
+                CommonConstants.DB_URL, CommonConstants.DB_USERNAME, CommonConstants.DB_PASSWORD);
+             PreparedStatement adminIDStmt = connection.prepareStatement(
+                     "SELECT AdminID FROM " + CommonConstants.DB_ADMIN_TABLE + " WHERE AdminName = ?");
+             PreparedStatement locationIDStmt = connection.prepareStatement(
+                     "SELECT AirportID FROM " + CommonConstants.DB_AIRPORTS_TABLE + " WHERE AirportName = ?");
+             PreparedStatement modifyPlaneStmt = connection.prepareStatement(modifyPlaneSQL)) {
 
             adminIDStmt.setString(1, updatedBy);
-            ResultSet resultSet = adminIDStmt.executeQuery();
+            ResultSet adminResultSet = adminIDStmt.executeQuery();
             Integer adminID = null;
 
-            if (resultSet.next()) {
-                adminID = resultSet.getInt("AdminID");
+            if (adminResultSet.next()) {
+                adminID = adminResultSet.getInt("AdminID");
             } else {
-                System.err.println("Admin not found");
+                System.err.println("Admin not found: " + updatedBy);
+                return false;
+            }
+
+            locationIDStmt.setString(1, locationAirport);
+            ResultSet locationResultSet = locationIDStmt.executeQuery();
+            Integer locationID = null;
+
+            if (locationResultSet.next()) {
+                locationID = locationResultSet.getInt("AirportID");
+            } else {
+                System.err.println("Airport not found: " + locationAirport);
                 return false;
             }
 
             modifyPlaneStmt.setString(1, model);
             modifyPlaneStmt.setInt(2, seats);
-            if (adminID != null) {
-                modifyPlaneStmt.setInt(3, adminID);
-            } else {
-                modifyPlaneStmt.setNull(3, java.sql.Types.INTEGER);
-            }
-            modifyPlaneStmt.setDate(4, new java.sql.Date(System.currentTimeMillis()));
-            modifyPlaneStmt.setInt(5, planeID);
+            modifyPlaneStmt.setInt(3, locationID);
+            modifyPlaneStmt.setInt(4, adminID);
+            modifyPlaneStmt.setDate(5, new java.sql.Date(System.currentTimeMillis()));
+            modifyPlaneStmt.setInt(6, planeID);
 
             int rowsAffected = modifyPlaneStmt.executeUpdate();
             return rowsAffected > 0;
@@ -91,12 +114,13 @@ public class Planes {
         }
     }
 
+
     public static boolean deletePlane(int planeID) {
         String deletePlaneSQL = "DELETE FROM " + CommonConstants.DB_PLANES_TABLE + " WHERE PlaneID = ?";
 
         try (Connection connection = DriverManager.getConnection(
-            CommonConstants.DB_URL, CommonConstants.DB_USERNAME, CommonConstants.DB_PASSWORD);
-            PreparedStatement deletePlaneStmt = connection.prepareStatement(deletePlaneSQL)) {
+                CommonConstants.DB_URL, CommonConstants.DB_USERNAME, CommonConstants.DB_PASSWORD);
+             PreparedStatement deletePlaneStmt = connection.prepareStatement(deletePlaneSQL)) {
 
             deletePlaneStmt.setInt(1, planeID);
             int rowsAffected = deletePlaneStmt.executeUpdate();
@@ -108,13 +132,37 @@ public class Planes {
         }
     }
 
-    public static ArrayList<ArrayList<Object>> viewPlane() {
-        String viewPlaneSQL = "SELECT * FROM " + CommonConstants.DB_PLANES_TABLE;
+    public static ArrayList<ArrayList<Object>> viewPlane(String model, String locationAirport, String updatedBy) {
+        StringBuilder viewPlaneSQL = new StringBuilder(
+                "SELECT p.PlaneID, p.Model, p.Seats, a.AirportName AS LocationAirport, " +
+                        "p.UpdatedBy, p.UpdatedDate " +
+                        "FROM " + CommonConstants.DB_PLANES_TABLE + " p " +
+                        "JOIN " + CommonConstants.DB_AIRPORTS_TABLE + " a ON p.LocationID = a.AirportID WHERE 1=1"
+        );
+
         ArrayList<ArrayList<Object>> planesList = new ArrayList<>();
+        ArrayList<Object> parameters = new ArrayList<>();
+
+        if (model != null && !model.isEmpty()) {
+            viewPlaneSQL.append(" AND p.Model LIKE ?");
+            parameters.add("%" + model + "%");
+        }
+        if (locationAirport != null && !locationAirport.isEmpty()) {
+            viewPlaneSQL.append(" AND a.AirportName LIKE ?");
+            parameters.add("%" + locationAirport + "%");
+        }
+        if (updatedBy != null && !updatedBy.isEmpty()) {
+            viewPlaneSQL.append(" AND p.UpdatedBy = ?");
+            parameters.add(updatedBy);
+        }
 
         try (Connection connection = DriverManager.getConnection(
-            CommonConstants.DB_URL, CommonConstants.DB_USERNAME, CommonConstants.DB_PASSWORD);
-            PreparedStatement viewPlanesStmt = connection.prepareStatement(viewPlaneSQL)) {
+                CommonConstants.DB_URL, CommonConstants.DB_USERNAME, CommonConstants.DB_PASSWORD);
+             PreparedStatement viewPlanesStmt = connection.prepareStatement(viewPlaneSQL.toString())) {
+
+            for (int i = 0; i < parameters.size(); i++) {
+                viewPlanesStmt.setObject(i + 1, parameters.get(i));
+            }
 
             ResultSet resultSet = viewPlanesStmt.executeQuery();
             while (resultSet.next()) {
@@ -122,7 +170,8 @@ public class Planes {
                 planeData.add(resultSet.getInt("PlaneID"));
                 planeData.add(resultSet.getString("Model"));
                 planeData.add(resultSet.getInt("Seats"));
-                planeData.add(resultSet.getObject("UpdatedBy")); // Nullable foreign key
+                planeData.add(resultSet.getString("LocationAirport"));
+                planeData.add(resultSet.getObject("UpdatedBy"));
                 planeData.add(resultSet.getDate("UpdatedDate"));
                 planesList.add(planeData);
             }
@@ -130,6 +179,9 @@ public class Planes {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+
         return planesList;
     }
+
+
 }
