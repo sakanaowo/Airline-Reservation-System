a8eb2ba8001c47d375e9fdc1d7a8bbad8846aea8
sakanaowo <anhlandibo88@gmail.com>
2024-11-20 23:56:33 +0700

    update DB design

--- src/DataHandle/DBdesign.sql
+++ src/DataHandle/DBdesign.sql
@@ -5,7 +5,7 @@ CREATE TABLE `airline`.`airports`
     `AirportID`   INT         NOT NULL AUTO_INCREMENT,
     `AirportName` VARCHAR(45) NOT NULL,
     `City`        VARCHAR(45) NOT NULL,
-    `Country`     VARCHAR(45) NOT NULL,
+    `AirportCode` VARCHAR(45) NOT NULL,
     PRIMARY KEY (`AirportID`)
 );
 
@@ -23,6 +23,7 @@ CREATE TABLE `airline`.`planes`
     `PlaneID`     INT         NOT NULL AUTO_INCREMENT,
     `Model`       VARCHAR(45) NOT NULL,
     `Seats`       INT         NOT NULL,
+    `LocationID`  INT         NOT NULL,
     `UpdatedBy`   INT NULL,
     `UpdatedDate` DATE        NOT NULL,
     PRIMARY KEY (`PlaneID`),
@@ -30,6 +31,11 @@ CREATE TABLE `airline`.`planes`
         FOREIGN KEY (`UpdatedBy`)
             REFERENCES `airline`.`admins` (`AdminID`)
             ON DELETE SET NULL
+            ON UPDATE CASCADE,
+    CONSTRAINT `fk_locationid_planes`
+        FOREIGN KEY (`LocationID`)
+            REFERENCES `airline`.`airports` (`AirportID`)
+            ON DELETE CASCADE
             ON UPDATE CASCADE
 );
 
@@ -53,9 +59,9 @@ CREATE TABLE `airline`.`flights`
             REFERENCES `airline`.`planes` (`PlaneID`)
             ON DELETE CASCADE
             ON UPDATE CASCADE,
-    CONSTRAINT `fk_departureairportid`
+    CONSTRAINT `fk_departureairportid_flights`
         FOREIGN KEY (`DepartureAirportID`)
-            REFERENCES `airline`.`airports` (`AirportID`)
+            REFERENCES `airline`.`planes` (`LocationID`)
             ON DELETE CASCADE
             ON UPDATE CASCADE,
     CONSTRAINT `fk_arrivalairportid`
@@ -69,6 +75,7 @@ CREATE TABLE `airline`.`flights`
             ON DELETE SET NULL
             ON UPDATE CASCADE
 );
+
 
 CREATE TABLE `airline`.`seats`
 (
@@ -134,82 +141,65 @@ INSERT INTO `airline`.`admins` (`AdminName`, `Password`, `Email`)
 VALUES ('thinh', SHA2('1', 256), 'thinhthiennguyen2004@gmail.com');
 
 
-INSERT INTO `airline`.`airports` (`AirportName`, `City`, `Country`)
-VALUES ('Noi Bai International Airport', 'Ha Noi', 'Vietnam'),
-       ('Tan Son Nhat International Airport', 'Ho Chi Minh', 'Vietnam'),
-       ('Da Nang International Airport', 'Da Nang', 'Vietnam'),
-       ('Cam Ranh International Airport', 'Nha Trang', 'Vietnam'),
-       ('Phu Quoc International Airport', 'Phu Quoc', 'Vietnam'),
-       ('Cat Bi International Airport', 'Hai Phong', 'Vietnam'),
-       ('Vinh International Airport', 'Vinh', 'Vietnam'),
-       ('Can Tho International Airport', 'Can Tho', 'Vietnam'),
-       ('Chu Lai Airport', 'Quang Nam', 'Vietnam'),
-       ('Dong Hoi Airport', 'Quang Binh', 'Vietnam');
+INSERT INTO `airline`.`airports` (`AirportName`, `City`, `AirportCode`)
+VALUES ('San bay Chu Lai', 'Quang Nam', 'VCL'),
+       ('San bay Tho Xuan', 'Thanh Hoa', 'THD'),
+       ('San bay Dong Hoi', 'Quang Binh', 'VDH'),
+       ('San bay Dien Bien Phu', 'Dien Bien', 'DIN'),
+       ('San bay Tuy Hoa', 'Phu Yen', 'TBB'),
+       ('San bay Pleiku', 'Gia Lai', 'PXU'),
+       ('San bay Buon Me Thuot', 'Dak Lak', 'BMV'),
+       ('San bay Rach Gia', 'Kien Giang', 'VKG'),
+       ('San bay Ca Mau', 'Ca Mau', 'CAH'),
+       ('San bay Con Dao', 'Ba Ria – Vung Tau', 'VCS'),
+       ('San bay quoc te Noi Bai', 'Ha Noi', 'HAN'),
+       ('San bay quoc te Tan Son Nhat', 'Thanh pho Ho Chi Minh', 'SGN'),
+       ('San bay quoc te Phu Quoc', 'Kien Giang', 'PQC'),
+       ('San bay quoc te Da Nang', 'Da Nang', 'DAD'),
+       ('San bay quoc te Cam Ranh', 'Khanh Hoa', 'CXR'),
+       ('San bay quoc te Phu Bai', 'Thua Thien Hue', 'HUI'),
+       ('San bay quoc te Van Don', 'Quang Ninh', 'VDO'),
+       ('San bay quoc te Can Tho', 'Can Tho', 'VCA'),
+       ('San bay quoc te Vinh', 'Nghe An', 'VII'),
+       ('San bay Quoc te Phu Cat', 'Binh Dinh', 'UIH'),
+       ('San bay Quoc te Cat Bi', 'Hai Phong', 'HPH'),
+       ('San bay Quoc te Lien Khuong', 'Lam Dong', 'DLI');
 
-INSERT INTO `airline`.`planes` (`Model`, `Seats`, `UpdatedBy`, `UpdatedDate`)
-VALUES ('Airbus A320', 180, 1, CURDATE()),
-       ('Airbus A320', 180, 1, CURDATE()),
-       ('Airbus A320', 180, 1, CURDATE()),
-       ('Airbus A320', 180, 1, CURDATE()),
-       ('Airbus A320', 180, 1, CURDATE()),
-       ('Airbus A320', 180, 1, CURDATE()),
-       ('Airbus A320', 180, 1, CURDATE()),
-       ('Airbus A320', 180, 1, CURDATE()),
-       ('Airbus A320', 180, 1, CURDATE()),
-       ('Airbus A320', 180, 1, CURDATE()),
-       ('Airbus A320', 180, 1, CURDATE()),
-       ('Airbus A320', 180, 1, CURDATE());
+INSERT INTO `airline`.`planes` (`Model`, `Seats`, `UpdatedBy`, `UpdatedDate`, `LocationID`)
+VALUES ('Airbus A320', 180, 1, CURDATE(), 1),
+       ('Airbus A320', 180, 1, CURDATE(), 2),
+       ('Airbus A320', 180, 1, CURDATE(), 3),
+       ('Airbus A320', 180, 1, CURDATE(), 4),
+       ('Airbus A320', 180, 1, CURDATE(), 5),
+       ('Airbus A320', 180, 1, CURDATE(), 6),
+       ('Airbus A320', 180, 1, CURDATE(), 7),
+       ('Airbus A320', 180, 1, CURDATE(), 8),
+       ('Airbus A320', 180, 1, CURDATE(), 9),
+       ('Airbus A320', 180, 1, CURDATE(), 10);
 
-
-INSERT INTO `airline`.`flights`
-(`DepartureTime`, `ArrivalTime`, `PlaneID`, `DepartureAirportID`, `ArrivalAirportID`, `UpdatedBy`, `UpdatedDate`)
+INSERT INTO `airline`.`flights` (`DepartureTime`, `ArrivalTime`, `PlaneID`, `DepartureAirportID`, `ArrivalAirportID`,
+                                 `UpdatedBy`, `UpdatedDate`)
 VALUES (DATE_ADD(CURDATE(), INTERVAL 1 DAY) + INTERVAL '08:00:00' HOUR_SECOND,
-        DATE_ADD(CURDATE(), INTERVAL 1 DAY) + INTERVAL '10:00:00' HOUR_SECOND,
-        1, 1, 2, 1, CURDATE()),
+        DATE_ADD(CURDATE(), INTERVAL 1 DAY) + INTERVAL '12:00:00' HOUR_SECOND, 1, 1, 2, 1, CURDATE()),
+       (DATE_ADD(CURDATE(), INTERVAL 1 DAY) + INTERVAL '09:00:00' HOUR_SECOND,
+        DATE_ADD(CURDATE(), INTERVAL 1 DAY) + INTERVAL '13:00:00' HOUR_SECOND, 2, 2, 3, 1, CURDATE()),
+       (DATE_ADD(CURDATE(), INTERVAL 1 DAY) + INTERVAL '10:00:00' HOUR_SECOND,
+        DATE_ADD(CURDATE(), INTERVAL 1 DAY) + INTERVAL '14:00:00' HOUR_SECOND, 3, 3, 4, 1, CURDATE()),
+       (DATE_ADD(CURDATE(), INTERVAL 1 DAY) + INTERVAL '11:00:00' HOUR_SECOND,
+        DATE_ADD(CURDATE(), INTERVAL 1 DAY) + INTERVAL '15:00:00' HOUR_SECOND, 4, 4, 5, 1, CURDATE()),
+       (DATE_ADD(CURDATE(), INTERVAL 1 DAY) + INTERVAL '12:00:00' HOUR_SECOND,
+        DATE_ADD(CURDATE(), INTERVAL 1 DAY) + INTERVAL '16:00:00' HOUR_SECOND, 5, 5, 6, 1, CURDATE()),
 
+       (DATE_ADD(CURDATE(), INTERVAL 2 DAY) + INTERVAL '08:00:00' HOUR_SECOND,
+        DATE_ADD(CURDATE(), INTERVAL 2 DAY) + INTERVAL '12:00:00' HOUR_SECOND, 6, 6, 7, 1, CURDATE()),
        (DATE_ADD(CURDATE(), INTERVAL 2 DAY) + INTERVAL '09:00:00' HOUR_SECOND,
-        DATE_ADD(CURDATE(), INTERVAL 2 DAY) + INTERVAL '11:30:00' HOUR_SECOND,
-        2, 6, 3, 1, CURDATE()),
-
-       (DATE_ADD(CURDATE(), INTERVAL 1 DAY) + INTERVAL '08:00:00' HOUR_SECOND,
-        DATE_ADD(CURDATE(), INTERVAL 1 DAY) + INTERVAL '10:30:00' HOUR_SECOND,
-        3, 1, 2, 1, CURDATE()),
-
-       (DATE_ADD(CURDATE(), INTERVAL 2 DAY) + INTERVAL '09:00:00' HOUR_SECOND,
-        DATE_ADD(CURDATE(), INTERVAL 2 DAY) + INTERVAL '12:00:00' HOUR_SECOND,
-        4, 2, 3, 1, CURDATE()),
-
-       (DATE_ADD(CURDATE(), INTERVAL 3 DAY) + INTERVAL '07:00:00' HOUR_SECOND,
-        DATE_ADD(CURDATE(), INTERVAL 3 DAY) + INTERVAL '10:15:00' HOUR_SECOND,
-        5, 3, 4, 1, CURDATE()),
-
-       (DATE_ADD(CURDATE(), INTERVAL 4 DAY) + INTERVAL '10:00:00' HOUR_SECOND,
-        DATE_ADD(CURDATE(), INTERVAL 4 DAY) + INTERVAL '13:00:00' HOUR_SECOND,
-        6, 4, 5, 1, CURDATE()),
-
-       (DATE_ADD(CURDATE(), INTERVAL 5 DAY) + INTERVAL '06:00:00' HOUR_SECOND,
-        DATE_ADD(CURDATE(), INTERVAL 5 DAY) + INTERVAL '09:30:00' HOUR_SECOND,
-        7, 5, 6, 1, CURDATE()),
-
-       (DATE_ADD(CURDATE(), INTERVAL 6 DAY) + INTERVAL '11:00:00' HOUR_SECOND,
-        DATE_ADD(CURDATE(), INTERVAL 6 DAY) + INTERVAL '13:45:00' HOUR_SECOND,
-        8, 6, 7, 1, CURDATE()),
-
-       (DATE_ADD(CURDATE(), INTERVAL 7 DAY) + INTERVAL '08:30:00' HOUR_SECOND,
-        DATE_ADD(CURDATE(), INTERVAL 7 DAY) + INTERVAL '12:00:00' HOUR_SECOND,
-        9, 7, 8, 1, CURDATE()),
-
-       (DATE_ADD(CURDATE(), INTERVAL 1 DAY) + INTERVAL '14:00:00' HOUR_SECOND,
-        DATE_ADD(CURDATE(), INTERVAL 1 DAY) + INTERVAL '16:30:00' HOUR_SECOND,
-        10, 8, 9, 1, CURDATE()),
-
-       (DATE_ADD(CURDATE(), INTERVAL 2 DAY) + INTERVAL '17:00:00' HOUR_SECOND,
-        DATE_ADD(CURDATE(), INTERVAL 2 DAY) + INTERVAL '20:00:00' HOUR_SECOND,
-        11, 9, 10, 1, CURDATE()),
-
-       (DATE_ADD(CURDATE(), INTERVAL 3 DAY) + INTERVAL '15:30:00' HOUR_SECOND,
-        DATE_ADD(CURDATE(), INTERVAL 3 DAY) + INTERVAL '18:45:00' HOUR_SECOND,
-        12, 10, 1, 1, CURDATE());
+        DATE_ADD(CURDATE(), INTERVAL 2 DAY) + INTERVAL '13:00:00' HOUR_SECOND, 7, 7, 8, 1, CURDATE()),
+       (DATE_ADD(CURDATE(), INTERVAL 2 DAY) + INTERVAL '10:00:00' HOUR_SECOND,
+        DATE_ADD(CURDATE(), INTERVAL 2 DAY) + INTERVAL '14:00:00' HOUR_SECOND, 8, 8, 9, 1, CURDATE()),
+       (DATE_ADD(CURDATE(), INTERVAL 2 DAY) + INTERVAL '11:00:00' HOUR_SECOND,
+        DATE_ADD(CURDATE(), INTERVAL 2 DAY) + INTERVAL '15:00:00' HOUR_SECOND, 9, 9, 10, 1, CURDATE()),
+       (DATE_ADD(CURDATE(), INTERVAL 2 DAY) + INTERVAL '12:00:00' HOUR_SECOND,
+        DATE_ADD(CURDATE(), INTERVAL 2 DAY) + INTERVAL '16:00:00' HOUR_SECOND, 10, 10, 1, 1, CURDATE());
 
 USE
 airline;
@@ -241,7 +231,7 @@ seatLetters CHAR(1) DEFAULT 'A';
 
     WHILE
 businessSeatsInserted < totalBusinessSeats DO
-        SET seatLetters = CHAR(65 + (businessSeatsInserted % 6)); 
+        SET seatLetters = CHAR(65 + (businessSeatsInserted % 6));
         SET
 position = CONCAT(seatLetters, FLOOR(businessSeatsInserted / 6) + 1);
 
@@ -256,7 +246,7 @@ END WHILE;
     WHILE
 economySeatsInserted < totalEconomySeats DO
 
-        SET seatLetters = CHAR(65 + (economySeatsInserted % 6)); 
+        SET seatLetters = CHAR(65 + (economySeatsInserted % 6));
         SET
 position = CONCAT(seatLetters, FLOOR(economySeatsInserted / 6) + 1);
 
@@ -281,34 +271,6 @@ CALL InsertSeats(7, 1500000, 500000, 1, CURDATE());
 CALL InsertSeats(8, 1500000, 500000, 1, CURDATE());
 CALL InsertSeats(9, 1500000, 500000, 1, CURDATE());
 CALL InsertSeats(10, 1500000, 500000, 1, CURDATE());
-CALL InsertSeats(11, 1500000, 500000, 1, CURDATE());
-CALL InsertSeats(12, 1500000, 500000, 1, CURDATE());
 
--- thay doi ten column va cap nhat
-delete
-from airports
-where AirportID in (1, 11);
-ALTER TABLE `airports` CHANGE COLUMN `Country` `AirportCode` VARCHAR (10) NOT NULL;
-INSERT INTO `airline`.`airports` (`AirportName`, `City`, `AirportCode`)
-VALUES ('San bay Chu Lai', 'Quang Nam', 'VCL'),
-       ('San bay Tho Xuan', 'Thanh Hoa', 'THD'),
-       ('San bay Dong Hoi', 'Quang Binh', 'VDH'),
-       ('San bay Dien Bien Phu', 'Dien Bien', 'DIN'),
-       ('San bay Tuy Hoa', 'Phu Yen', 'TBB'),
-       ('San bay Pleiku', 'Gia Lai', 'PXU'),
-       ('San bay Buon Me Thuot', 'Dak Lak', 'BMV'),
-       ('San bay Rach Gia', 'Kien Giang', 'VKG'),
-       ('San bay Ca Mau', 'Ca Mau', 'CAH'),
-       ('San bay Con Dao', 'Ba Ria – Vung Tau', 'VCS'),
-       ('San bay quoc te Noi Bai', 'Ha Noi', 'HAN'),
-       ('San bay quoc te Tan Son Nhat', 'Thanh pho Ho Chi Minh', 'SGN'),
-       ('San bay quoc te Phu Quoc', 'Kien Giang', 'PQC'),
-       ('San bay quoc te Da Nang', 'Da Nang', 'DAD'),
-       ('San bay quoc te Cam Ranh', 'Khanh Hoa', 'CXR'),
-       ('San bay quoc te Phu Bai', 'Thua Thien Hue', 'HUI'),
-       ('San bay quoc te Van Don', 'Quang Ninh', 'VDO'),
-       ('San bay quoc te Can Tho', 'Can Tho', 'VCA'),
-       ('San bay quoc te Vinh', 'Nghe An', 'VII'),
-       ('San bay Quoc te Phu Cat', 'Binh Dinh', 'UIH'),
-       ('San bay Quoc te Cat Bi', 'Hai Phong', 'HPH'),
-       ('San bay Quoc te Lien Khuong (dang de xuat)', 'Lam Dong', 'DLI');
+-- -- Chay lenh nay de xoa database
+-- DROP DATABASE `airline`;
