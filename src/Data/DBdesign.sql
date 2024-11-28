2f7469257546e85d2a90d97f59af76427d4f5eea
thinhnguyenpy <thinhthiennguyen2004@gmail.com>
2024-11-28 11:16:28 +0700

    them viewAirport

--- src/Data/DBdesign.sql
+++ src/Data/DBdesign.sql
@@ -119,7 +119,6 @@ CREATE TABLE `airline`.`tickets` (
   `TicketID` INT NOT NULL AUTO_INCREMENT,
   `PassengerID` INT NOT NULL,
   `SeatID` INT NOT NULL,
-  `Status` VARCHAR(45) NOT NULL,
   `TicketCode` VARCHAR(45) NOT NULL,
   `ReservationDate` DATETIME NOT NULL,
   PRIMARY KEY (`TicketID`),
