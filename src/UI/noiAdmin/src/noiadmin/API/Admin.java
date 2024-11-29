9cd25c097cb504b0dcf08c5e0c465c1cdcd14034
Dang Khoa <hnkhoa04@gmail.com>
2024-11-29 13:51:50 +0700

    update login

--- src/UI/noiAdmin/src/noiadmin/API/Admin.java
+++ src/UI/noiAdmin/src/noiadmin/API/Admin.java
@@ -9,15 +9,15 @@ import java.sql.*;
 
 
 public class Admin {
-    public static boolean checkAdminLogin(String username, String password) {
-        String sql = "SELECT Password FROM " + CommonConstants.DB_ADMIN_TABLE + " WHERE AdminName = ?";
+    public static boolean checkAdminLogin(String email, String password) {
+        String sql = "SELECT Password FROM " + CommonConstants.DB_ADMIN_TABLE + " WHERE Email = ?";
         boolean isAuthenticated = false;
 
         try (Connection connection = DriverManager.getConnection(
                 CommonConstants.DB_URL, CommonConstants.DB_USERNAME, CommonConstants.DB_PASSWORD);
             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
 
-            preparedStatement.setString(1, username);
+            preparedStatement.setString(1, email);
 
             try (ResultSet resultSet = preparedStatement.executeQuery()) {
                 if (resultSet.next()) {
