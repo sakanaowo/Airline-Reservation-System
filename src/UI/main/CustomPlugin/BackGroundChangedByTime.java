c57e955aba93b50689ab6583ad7008c8e768dbbc
sakanaowo <anhlandibo88@gmail.com>
2024-11-30 10:23:19 +0700

    add plugin

---
+++ src/UI/main/CustomPlugin/BackGroundChangedByTime.java
@@ -0,0 +1,36 @@
+package UI.main.CustomPlugin;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class BackGroundChangedByTime extends JPanel {
+    private Image[] images;
+    private int currentIndex = 0;
+
+    public BackGroundChangedByTime() {
+        images = new Image[]{
+                new ImageIcon(getClass().getResource("/UI/Icon/phu-quoc.jpg")).getImage(),
+                new ImageIcon(getClass().getResource("/UI/Icon/bg.jpg")).getImage(),
+                new ImageIcon(getClass().getResource("/UI/Icon/hcml-1.jpg")).getImage()
+        };
+        Timer timer = new Timer(3000, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                currentIndex = (currentIndex + 1) % images.length;
+                repaint();
+            }
+        });
+        timer.start();
+    }
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC);  // Làm mịn ảnh khi phóng to
+
+        g2d.drawImage(images[currentIndex], 0, 0, getWidth(), getHeight(), this);
+    }
+}
---
+++ src/main.java
@@ -0,0 +1,10 @@
+import UI.main.Login;
+
+public class main {
+    public static void main(String[] args) {
+        Login l = new Login();
+        l.setVisible(true);
+        l.pack();
+        l.setLocationRelativeTo(null);
+    }
+}
