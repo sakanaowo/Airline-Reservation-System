5cbeab1e10c792ca1d4f9b38044b5232e418354c
sakanaowo <anhlandibo88@gmail.com>
2024-12-01 21:20:41 +0700

    update

--- src/UI/main/ConfirmationFrame.java
+++ src/UI/main/ConfirmationFrame.java
@@ -582,10 +582,6 @@ public class ConfirmationFrame extends javax.swing.JFrame {
             java.util.logging.Logger.getLogger(ConfirmationFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
